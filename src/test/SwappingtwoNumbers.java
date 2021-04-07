12005e9ba23b25393088ffbf713b2ccf17fa2f71
MoumitaRB <64107855+MoumitaRB@users.noreply.github.com>
2021-04-07 16:13:43 -0500

    Update SwappingtwoNumbers.java

--- src/test/SwappingtwoNumbers.java
+++ src/test/SwappingtwoNumbers.java
@@ -5,7 +5,8 @@ public class SwappingtwoNumbers {
         int a=30;
         int b=20;
         int c=0;
-
+        System.out.println("Before swipping numbers the value of a is: " +a);
+        System.out.println("Before swipping numbers the value of b is: "+b);
         c=a;
         a=b;
         b=c;
