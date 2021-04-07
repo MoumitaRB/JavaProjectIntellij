95a562c7ec9a9509412088e433efbcdf6daa87ad
MoumitaRB <moumitabasak@gmail.com>
2021-04-07 16:00:28 -0500

    "Updated project..."

---
+++ src/test/SwappingtwoNumbers.java
@@ -0,0 +1,16 @@
+package test;
+
+public class SwappingtwoNumbers {
+    public static void main(String[] args) {
+        int a=30;
+        int b=20;
+        int c=0;
+
+        c=a;
+        a=b;
+        b=c;
+
+        System.out.println("after swapping numbers the value of a is: "+ a);
+        System.out.println("After swapping numbers the value of b is: "+ b);
+    }
+}
